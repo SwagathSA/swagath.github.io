21187369e149ced60f8737c483c7f7c878f53bf8
SwagathSA <137690721+SwagathSA@users.noreply.github.com>
2023-10-26 20:22:12 +0530

    Add files via upload

---
+++ Webprojects/scripts/main.js
@@ -0,0 +1,44 @@
+// const myHeading = document.querySelector("h1");
+// myHeading.textContent = "Hello evryone!"
+
+
+
+
+
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "./images/trial1.jpg"){
+        myImage.setAttribute("src","./images/trial2.jpg" );
+    } else {
+        myImage.setAttribute("src","./images/trial1.jpg")
+    }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName(){
+    const myName = prompt("Please enter your name");
+    if(!myName){
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `Mozilla is cool, ${myName}.`;
+    }
+    // localStorage.setItem("name",myName);
+    // myHeading.textContent = `Mozilla is cool, ${myName} `;
+}
+
+if(!localStorage.getItem("name")){
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.onclick = () => {
+    setUserName();
+}
+
